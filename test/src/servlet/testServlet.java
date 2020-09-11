bdf6c5fe20cafc4c7b973af5e94ac5fb12047300
chil13 <matsuo.mbc6@gmail.com>
2020-09-12 06:30:47 +0900

    aaaa

--- test/src/servlet/testServlet.java
+++ test/src/servlet/testServlet.java
@@ -2,6 +2,7 @@ package servlet;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,25 +17,13 @@ public class testServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public testServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/jsp/test.jsp");
+		dispatcher.forward(request, response);
+		return;
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
